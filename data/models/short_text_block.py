a59e26df917e3f8a5a5b2cf132e61dce91b6df6f
B4nanaJuice <griesmaxime2@gmail.com>
2026-03-12 10:21:02 +0100

    Fixed ShortTextBlock class and added ShortTextAnswer class

--- data/models/short_text_block.py
+++ data/models/short_text_block.py
@@ -1,18 +1,36 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 from data.database import db
 from data.models.form_block import FormBlock
 
-class SmallTextBlock(FormBlock):
-    __tablename__ = 'small_text_block'
+class ShortTextBlock(FormBlock):
+    __tablename__ = 'short_text_block'
 
     # Attributes
     id: Mapped[int] = mapped_column(ForeignKey('form_block.id'), primary_key = True)
 
+    answers: Mapped[List['ShortTextAnswer']] = relationship(
+        cascade = 'all, delete-orphan'
+    )
+
     __mapper_args__ = {
-        'polymorphic_identity': 'small_text_block'
+        'polymorphic_identity': 'short_text_block'
     }
 
     def __repr__(self):
-        return f'<SmallTextBlock {self.title}>'
+        return f'<ShortTextBlock {self.title}>'
+    
+class ShortTextAnswer(db.Model):
+    __tablename__ = 'short_text_answer'
+
+    # Attributes
+    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
+
+    value = Mapped[str] = mapped_column(nullable = True)
+    
+    short_text_block_id: Mapped[int] = mapped_column(ForeignKey('short_text_block.id'))
+
+    def __repr__(self):
+        return f'<ShortTextAnswer {self.value}>'
